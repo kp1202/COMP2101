47635bbdd0ad42a34259bc4176973fa5665d5dfa
Krisha Patel <200477863@student.georgianc.on.ca>
2022-06-06 00:34:35 -0400

    created file

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,12 +1,15 @@
-$ ./sysinfo.sh
-Host Name: Krisha-virtual-machine
-Domain Name: home.krisha
-Operating System name and version: PRETTY_NAME="Debian GNU/Linux 10 (buster)"
-IP Addresses: 192.168.47.128 fe80::afce:8452:832:d980
-Root filesystem status:
+#!/bin/bash
+# $ ./sysinfo.sh
+
+echo 'Host Name: Krisha-virtual-machine'
+echo 'Domain Name: home.krisha'
+echo 'Operating System name and version: 
+PRETTY_NAME="Debian GNU/Linux 10 (buster)"'
+echo 'IP Addresses: 192.168.47.128 fe80::afce:8452:832:d980'
+echo 'Root filesystem status:
 	Filesystem - /dev/sdal
 	Size - 12G
 	Used - 5.6G
 	Avail - 5.6G
 	Use% - 50%
-	Mounted on- /
+	Mounted on- /'
