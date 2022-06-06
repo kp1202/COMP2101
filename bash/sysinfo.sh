3fa522be41a4eccb718230f1f56e24e1b5db2e11
Krisha Patel <200477863@student.georgianc.on.ca>
2022-06-06 00:10:43 -0400

    your commit comment

---
+++ bash/sysinfo.sh
@@ -0,0 +1,12 @@
+$ ./sysinfo.sh
+Host Name: Krisha-virtual-machine
+Domain Name: home.krisha
+Operating System name and version: PRETTY_NAME="Debian GNU/Linux 10 (buster)"
+IP Addresses: 192.168.47.128 fe80::afce:8452:832:d980
+Root filesystem status:
+	Filesystem - /dev/sdal
+	Size - 12G
+	Used - 5.6G
+	Avail - 5.6G
+	Use% - 50%
+	Mounted on- /
